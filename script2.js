7d238092e6c971a7a9c7ab46efd99508ef1aad88
Steven <buildpucipuci02@gmail.com>
2023-10-28 15:36:13 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,24 +1,10 @@
-
-
 const BUFFER_SIZE = 8192;
-// const Tone = require('tone');
 
 class AudioPlayer {
   constructor({emitter, pitch, tempo, reverbImpulseResponse}) {
       this.emitter = emitter;
 
       this.context = new AudioContext();
-
-        // // Create a ConvolverNode for reverb
-        // this.convolverNode = this.context.createConvolver();
-
-        // // Load an impulse response (IR) for reverb
-        // this.loadReverbImpulseResponse(reverbImpulseResponse);
-
-      // Tried to turn this into a 4 channel, it doesn't work cause
-      // audio files are stereo, even if mono, left and right will be the same
-      // When I made it into 4, it cut out every half a second and pitched it up
-
       this.scriptProcessor = this.context.createScriptProcessor(BUFFER_SIZE, 2, 2);
       this.scriptProcessor.onaudioprocess = e => {
           const l = e.outputBuffer.getChannelData(0);
@@ -33,9 +19,6 @@ class AudioPlayer {
           }
       };
 
-    //   this.audioSource.connect(this.convolverNode);
-    //   this.convolverNode.connect(this.scriptProcessor);
-    //   this.scriptProcessor.connect(this.context.destination);
      
       this.soundTouch = new SoundTouch();
       this.soundTouch.pitch = pitch;
@@ -43,51 +26,17 @@ class AudioPlayer {
       
       this.duration = undefined;
 
-        // Load impulse response and create ConvolverNode
-        // this.impulseResponseBuffer = null;
-        // this.urlValue = reverbImpulseResponse;
-        // fetch(this.urlValue)
-        //     .then((response) => response.arrayBuffer())
-        //     .then((data) => this.context.decodeAudioData(data))
-        //     .then((buffer) => {
-        //         this.impulseResponseBuffer = buffer;
-        //         this.convolverNode.buffer = buffer; // Set the ConvolverNode's buffer
-        //     })
-        //     .catch((error) => console.error('Error loading impulse response:', error));
         
-        this.volumeNode = this.context.createGain();
-        this.volumeNode.gain.value = 0.5;
-        
-        this.convolverNode = this.context.createConvolver();
-        this.convolverNode.buffer = reverbImpulseResponse;
+      this.volumeNode = this.context.createGain();
+      this.volumeNode.gain.value = 0.5;
+    
+      this.convolverNode = this.context.createConvolver();
+      this.convolverNode.buffer = reverbImpulseResponse;
 
-        this.dryWetGainNode = this.context.createGain();
-        this.dryWetGainNode.gain.value = 0.6;
-        
-        // this.reverb = new Tone.Reverb({
-        //     decay: 3, // Adjust the decay time as needed
-        //     wet: 0.4, // Adjust the wet/dry mix as needed (0 = dry, 1 = wet)
-        // }).toDestination();
-
-        // this.output = new Tone.Gain().toDestination();
-        // this.scriptProcessor.connect(this.reverb);
-        // this.reverb.connect(this.convolverNode);
-        // this.dryWetGainNode.connect(this.reverb); // Connect the GainNode to the context's destination
-        // this.reverb.connect(output);
-        
-        // Connect the nodes
-
-        // this.scriptProcessor.connect(this.convolverNode); // Connect the scriptProcessor to the ConvolverNode
-        // this.convolverNode.connect(this.dryWetGainNode); // Connect the ConvolverNode to the GainNode
-        // this.dryWetGainNode.connect(this.context.destination); // Connect the GainNode to the context's destination
-
-        // Pointless to connect it here
+      this.dryWetGainNode = this.context.createGain();
+      this.dryWetGainNode.gain.value = 0.6;
   }
 
-
-//   set fetchingURL(urlVal){
-//     return this.urlValue = urlVal;
-//   }
 
   setImpulseResponse(buffer) {
     if (this.convolverNode) {
@@ -140,54 +89,19 @@ class AudioPlayer {
       this.emitter.emit('state', {duration: buffer.duration});
   }
 
-    // setBuffer(buffer) {
-    //     const bufferSource = this.context.createBufferSource();
-    //     bufferSource.buffer = buffer;
-
-    //     this.samples = new Float32Array(BUFFER_SIZE * 2);
-    //     this.source = {
-    //         extract: (target, numFrames, position) => {
-    //             this.emitter.emit('state', { t: position / this.context.sampleRate });
-    //             const l = buffer.getChannelData(0);
-    //             const r = buffer.getChannelData(1);
-    //             for (let i = 0; i < numFrames; i++) {
-    //                 target[i * 2] = l[i + position];
-    //                 target[i * 2 + 1] = r[i + position];
-    //             }
-    //             return Math.min(numFrames, l.length - position);
-    //         },
-    //     };
-    //     this.simpleFilter = new SimpleFilter(this.source, this.soundTouch);
-
-    //     // Connect your audio source to the Tone.js reverb
-    //     bufferSource.connect(this.reverb);
-
-    //     this.duration = buffer.duration;
-    //     this.emitter.emit('state', { duration: buffer.duration });
-    // }
 
   play() {
         
       this.scriptProcessor.connect(this.volumeNode);
       this.volumeNode.connect(this.context.destination)
-    //   this.scriptProcessor.connect(this.context.destination);
+
       this.context.resume().then(() => {
               this.scriptProcessor.connect(this.convolverNode);
-            //   this.volumeNode.connect(this.convolverNode)
+
               this.convolverNode.connect(this.dryWetGainNode);
               this.dryWetGainNode.connect(this.context.destination);
           });
-    /*
-    this.scriptProcessor.connect(this.context.destination);
-  // this.scriptProcessor.connect(this.convolverNode); // Connect the scriptProcessor to the ConvolverNode
-  // this.convolverNode.connect(this.dryWetGainNode); // Connect the ConvolverNode to the GainNode
-  // this.dryWetGainNode.connect(this.context.destination); // Connect the GainNode to the context's destination
-    */
 }
-
-// playSlider(seekSlider){
-    
-// }
 
 pause() {
     this.scriptProcessor.disconnect(this.volumeNode);
@@ -195,26 +109,12 @@ pause() {
     this.scriptProcessor.disconnect(this.convolverNode);
     this.convolverNode.disconnect(this.dryWetGainNode);
     this.dryWetGainNode.disconnect(this.context.destination);
-    
-
-    /*
-
-        // This sounds kinda cool as a button:
-        // this.scriptProcessor.connect(this.context.destination); // Only this in play()
-
-        this.scriptProcessor.disconnect(this.context.disconnect);
-        this.scriptProcessor.connect(this.convolverNode); // Connect the scriptProcessor to the ConvolverNode
-        this.convolverNode.connect(this.dryWetGainNode); // Connect the ConvolverNode to the GainNode
-    
-    */
-
-  }
+}
 
 toggle(statement){
     if(statement == true){
         this.scriptProcessor.disconnect(this.volumeNode);
         this.volumeNode.disconnect(this.context.destination);
-        // this.scriptProcessor.disconnect(this.convolverNode);
         this.convolverNode.connect(this.dryWetGainNode);
         this.dryWetGainNode.connect(this.context.destination);
     }
@@ -253,12 +153,9 @@ const seekSlider = document.getElementById('seekSlider');
 const currentTimeDisplay = document.getElementById('currentTime');
 
 
-// const updateSeekInterval = setInterval(updateSeek, 1000);
 let myInterval;
 
 let isPlaying = false;
-// let duration = 0;
-// let precDuration = 0;
 
 let toggleRev = false;
 
@@ -267,9 +164,6 @@ let audioPlayer;
 let bufferOr;
 let audioData;
 
-
-// let impulse = returnImpulse(reverbInput);
-// let impulse = impulseResponse(3, 2);
 
 fileInput.addEventListener('change', async (e, impulse) => {
     // If there's a song selected we pause it and undefine
@@ -305,12 +199,11 @@ fileInput.addEventListener('change', async (e, impulse) => {
 
         audioPlayer.play();
         isPlaying = true;
-        // updateSeekInterval = setInterval(updateSeek(audioPlayer, seekSlider), 1000);
+        
         myInterval = setInterval(()=>{
             updateSeek(audioPlayer, seekSlider);
         }, 1000);
-        // console.log(audioPlayer.duration);
-        // duration = audioPlayer.duration;
+
     } catch (error) {
         console.error(error);
     }
@@ -368,9 +261,9 @@ revDecSlider.addEventListener('pointerup', ()=>{
 
 
 function impulseResponse(duration, decay, bChannels = bufferChannels) {
-    // console.log(bChannels);
+
     let chanAmount = returnBChannel(bChannels);
-    // console.log(chanAmount);
+
     let context = new AudioContext();
     let length = context.sampleRate * duration;
     let impulse = context.createBuffer(chanAmount, length, context.sampleRate); // first value is whether it's mono, stereo or 4-channel so either 1, 2, or 4
@@ -403,23 +296,13 @@ function impulseResponse(duration, decay, bChannels = bufferChannels) {
     return impulse;
   }
 
-// function returnImpulse(input){
-//     input.addEventListener('change', (e) =>{
-//         const file = e.target.files[0];
-//         const objectURL = URL.createObjectURL(file);
-
-//         return objectURL;
-//     })
-// }
-
 function returnBChannel(bufferChannels){
     let selectedVal = undefined;
     bufferChannels.forEach(radInp =>{
-        // console.log(radInp.value);
+
         if(radInp.checked == true){
             selectedVal = radInp.value;
-            // console.log(selectedVal);
-            // return radInp.value; RETURN OUT OF THE IF STATEMENT!
+
         }
     })
     return selectedVal;
@@ -446,10 +329,6 @@ playButton.addEventListener('click', () => {
     bufferOr = response.arrayBuffer();
     audioData = audioPlayer.decodeAudioData(bufferOr);
     audioPlayer.setBuffer(audioData);
-    // setInterval(() => {
-    //     precDuration += 1
-    //     seekSlider.value = precDuration / duration;
-    // }, 1000);
 });
 
 pauseButton.addEventListener('click', () => {
@@ -510,7 +389,6 @@ reverbSlider.addEventListener('input', () =>{
 
 bufferChannels.forEach(radInp =>{
     radInp.addEventListener('click', ()=>{
-        // console.log()
         let impulse = impulseResponse(revDurSlider.value, revDecSlider.value);
         audioPlayer.setImpulseResponse(impulse);
     })
@@ -526,43 +404,3 @@ seekSlider.addEventListener('input', ()=>{
     audioData = audioPlayer.decodeAudioData(bufferOr);
     audioPlayer.setBuffer(audioData);
 })
-
-
-/*
-
- => Duration: 131
- => Tempo: 1
-
- let duration = 0;
-
- if(play){
-    setInterval({
-        duration +=
-        seekbar.value = 
-    }, 1000)
- }
-
-
-*/
-
-
-
-// emitter: {
-//     emit: (event, data) => {
-//         if (event === 'state') {
-//             if (data.t !== undefined) {
-//                 // Update the current time display
-//                 const minutes = Math.floor(data.t / 60);
-//                 const seconds = Math.floor(data.t % 60);
-//                 currentTimeDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-//             }
-//             if (data.duration) {
-//                 // Update the duration display if available
-//                 // You can add a duration display element and update it here.
-//             }
-//         }
-//         if (event === 'stop') {
-//             playButton.textContent = 'Play';
-//         }
-//     },
-// },
